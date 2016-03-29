bba3cd5557bd84584c858c740fa8d0bc9a2b7bea
kushal <kushaldeveloper@gmail.com>
2016-03-29 10:34:53 -0400

    change email validation
    if the input email address is not valid, don't store it
    
    store an empty string for now
    
    perhaps store a generic email address for the observer magazine instead in the future?

--- Observer.PublicationBusiness/Author.cs
+++ Observer.PublicationBusiness/Author.cs
@@ -8,11 +8,26 @@ namespace Observer.PublicationBusiness
 {
     public class Author
     {
+        private string _email;
         public Guid Id { get; set; }
         public string FullName { get; set; }
         public string ShortName { get; set; }
         public string TwitterHandle { get; set; }
-        public string Email { get; set; }
+        public string Email {
+            get { return _email; }
+            set
+            {
+                var foo = new System.ComponentModel.DataAnnotations.EmailAddressAttribute();
+                if (foo.IsValid(value))
+                {
+                    _email = value;
+                }
+                else
+                {
+                    _email = "";
+                }
+            }
+        }
         public Author(string fullName, string shortName, string twitterHandle, string email)
         {
             this.Id = Guid.NewGuid();
@@ -21,5 +36,6 @@ namespace Observer.PublicationBusiness
             this.TwitterHandle = twitterHandle;
             this.Email = email;
         }
+
     }
 }
