b829ecf965c6c0b988390a55796cf9385d73ef1b
kushal <kushaldeveloper@gmail.com>
2016-03-28 20:51:10 -0400

    add what I want to do

--- ObserverConsole/Program.cs
+++ ObserverConsole/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, world!"); 
+            System.Console.WriteLine("Hello, world!");
+            //Observer.Business.Author kushalAuthor = new Observer.Business.Author("Kushal Hada", "kus", "9034725985", "kushaldeveloper@gmail.com");
         }
     }
 }
