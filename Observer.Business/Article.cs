1fde934224a7661a08f6e43d084b0d5a6c1b29f4
kushal <kushaldeveloper@gmail.com>
2016-03-28 20:04:01 -0400

    create a business project for my business objects

---
+++ Observer.Business/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Business
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string ArticleTitle { get; set; }
+        public string ArticleByLine { get; set; }
+        public List<Author> Authors { get; set; }
+    }
+}
---
+++ Observer.Business/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Business
+{
+    public class Author
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string ShortName { get; set; }
+        public string TwitterHandle { get; set; }
+        public string Email { get; set; }
+        public Author(string fullName, string shortName, string twitterHandle, string email)
+        {
+            this.Id = Guid.NewGuid();
+            this.FullName = fullName;
+            this.ShortName = shortName;
+            this.TwitterHandle = twitterHandle;
+            this.Email = email;
+        }
+    }
+}
