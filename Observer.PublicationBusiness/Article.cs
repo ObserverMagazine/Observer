1835a9326529929393208618ee6dae89015fffa4
kushal <kushaldeveloper@gmail.com>
2016-03-29 10:10:33 -0400

    Create a test for the constructor

--- Observer.PublicationBusiness/Article.cs
+++ Observer.PublicationBusiness/Article.cs
@@ -9,14 +9,20 @@ namespace Observer.PublicationBusiness
     public class Article
     {
         public Guid Id { get; set; }
-        public string ArticleTitle { get; set; }
-        public string ArticleByLine { get; set; }
+        public string Title { get; set; }
+        public string ByLine { get; set; }
         public List<Author> Authors { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="articleTitle">This is the title of the article. Try to keep it within 140 char</param>
+        /// <param name="articleByLine">This is the by line. Try to keep it within 80 char</param>
+        /// <param name="authors">This is the list of authors. Order matters. Do not sort.</param>
         public Article(string articleTitle, string articleByLine, List<Author> authors)
         {
             this.Id = Guid.NewGuid();
-            this.ArticleTitle = articleTitle;
-            this.ArticleByLine = articleByLine;
+            this.Title = articleTitle;
+            this.ByLine = articleByLine;
             this.Authors = authors;
         }
     }
