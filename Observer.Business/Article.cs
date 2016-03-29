a5b56264533a5f52d664f1e5f23265e7520eb26e
kushal <kushaldeveloper@gmail.com>
2016-03-28 20:42:45 -0400

    add constructor in article class

--- Observer.Business/Article.cs
+++ Observer.Business/Article.cs
@@ -12,5 +12,12 @@ namespace Observer.Business
         public string ArticleTitle { get; set; }
         public string ArticleByLine { get; set; }
         public List<Author> Authors { get; set; }
+        public Article(string articleTitle, string articleByLine, List<Author> authors)
+        {
+            this.Id = Guid.NewGuid();
+            this.ArticleTitle = articleTitle;
+            this.ArticleByLine = articleByLine;
+            this.Authors = authors;
+        }
     }
 }
