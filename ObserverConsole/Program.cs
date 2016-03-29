512acad1ea0fae9361daa2947c48849cf745cca1
kushal <kushaldeveloper@gmail.com>
2016-03-29 09:21:42 -0400

    add publication business as a reference in console app

--- ObserverConsole/Program.cs
+++ ObserverConsole/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello, world!");
-            //Observer.Business.Author kushalAuthor = new Observer.Business.Author("Kushal Hada", "kus", "9034725985", "kushaldeveloper@gmail.com");
+            Observer.PublicationBusiness.Author kushalAuthor = new Observer.PublicationBusiness.Author("Kushal Hada", "kus", "9034725985", "kushaldeveloper@gmail.com");
         }
     }
 }
