88d2b27c8316f36c24aa03e4b1cf3706d973a6e1
kushal <kushaldeveloper@gmail.com>
2016-03-29 09:14:22 -0400

    remove universal class library, add class library

--- Observer.PublicationBusiness/Properties/AssemblyInfo.cs
+++ Observer.PublicationBusiness/Properties/AssemblyInfo.cs
@@ -1,19 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Observer.Business")]
+[assembly: AssemblyTitle("Observer.PublicationBusiness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Observer.Business")]
+[assembly: AssemblyProduct("Observer.PublicationBusiness")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8cf8b4cb-b2ab-4850-8bad-fa8a0de7080b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -27,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
-
